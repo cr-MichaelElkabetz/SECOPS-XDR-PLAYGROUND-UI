ca24c784b53e2690b0e90b1bbcc6b53fe28414cd
Michael <michael.elkabetz@cybereason.com>
2021-10-21 12:39:11 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'xdr-playground-ui';
+}
